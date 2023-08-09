69c59d4046cdfd236580aa58a854aee73253a748
dikokartiko <dikowebdev@gmail.com>
2023-08-09 09:41:40 +0700

    refactors

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,42 +1,58 @@
 // controllers/transactions.js
 const { Transaction, Cart, Product } = require("../models");
 
+const getCartItems = async (cashierId) => {
+  return await Cart.findAll({
+    where: { cashierId },
+    include: Product,
+  });
+};
+
+const calculateTotalPrice = (cartItems) => {
+  let totalPrice = 0;
+  for (const item of cartItems) {
+    totalPrice += item.totalPrice;
+  }
+  return totalPrice;
+};
+
+const createTransaction = async (cashierId, amount, totalPrice, cartItems) => {
+  const change = amount - totalPrice;
+  for (const item of cartItems) {
+    await Transaction.create({
+      cashierId,
+      date: new Date(),
+      totalPrice,
+      amount,
+      change,
+      productId: item.productId,
+      quantity: item.quantity,
+      pricePerProduct: item.price,
+      totalPrice: item.totalPrice,
+    });
+    const product = await Product.findByPk(item.productId);
+    product.stock -= item.quantity;
+    await product.save();
+  }
+};
+
 exports.createTransaction = async (req, res) => {
   const { amount } = req.body;
   const cashierId = req.userId;
   try {
-    const cartItems = await Cart.findAll({
-      where: { cashierId },
-      include: Product,
-    });
-    let totalPrice = 0;
-    for (const item of cartItems) {
-      totalPrice += item.totalPrice;
-    }
+    const cartItems = await getCartItems(cashierId);
+    const totalPrice = calculateTotalPrice(cartItems);
+
     if (totalPrice === 0) {
       return res.status(400).send({ error: "cart cant be empty" });
     }
     if (amount < totalPrice) {
       return res.status(400).send({ error: "Insufficient amount" });
     }
-    const change = amount - totalPrice;
-    for (const item of cartItems) {
-      await Transaction.create({
-        cashierId,
-        date: new Date(),
-        totalPrice,
-        amount,
-        change,
-        productId: item.productId,
-        quantity: item.quantity,
-        pricePerProduct: item.price,
-        totalPrice: item.totalPrice,
-      });
-      const product = await Product.findByPk(item.productId);
-      product.stock -= item.quantity;
-      await product.save();
-    }
+
+    await createTransaction(cashierId, amount, totalPrice, cartItems);
     await Cart.destroy({ where: { cashierId } });
+
     res.status(201).send({ transaction: cartItems, change });
   } catch (error) {
     console.log(error);
